cf557a0fbdafe512dd7def5e06369f7e8fe4e4e4
VincentMoses123 <24.vincent.moses@gmail.com>
2024-03-22 11:53:15 -0400

    First Commit

---
+++ Currency_Converter.py
@@ -0,0 +1,16 @@
+from requests import get
+from pprint import PrettyPrinter
+
+BASE_URL = "https://free.currconv.com/"
+API_KEY = "9b99631642666327eb57"
+
+printer = PrettyPrinter()
+
+def get_currencies():
+    endpoint = f"api/v7/currencies?apiKey={API_KEY}"
+    url = BASE_URL + endpoint
+    data = get(url).json()
+    
+    printer.pprint(data)
+get_currencies()
+    
